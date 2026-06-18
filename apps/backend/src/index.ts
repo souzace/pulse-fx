777324125327d340bbf2b7b7429ca23f58787628
Fabio Souza <souzace@gmail.com>
2026-06-17 21:08:06 -0300

    chore: add winston

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,6 @@
 import { app } from './infra/http/app';
 import { startSyncScheduler } from './infra/cron/sync-scheduler';
+import { logger } from './infra/logger';
 
 const start = async () => {
   try {
@@ -7,9 +8,9 @@ const start = async () => {
     startSyncScheduler();
 
     await app.listen({ port: 3333, host: '0.0.0.0' });
-    console.log('HTTP Server Running on http://localhost:3333');
+    logger.info('HTTP Server Running on http://localhost:3333');
   } catch (error) {
-    console.error(error);
+    logger.error(error);
     process.exit(1);
   }
 };
